8d25ca557872de6e27599d691402dc3f90da9f28
Aleksey Kazakov <red_sea_pig@mail.ru>
2022-09-10 08:58:09 +0500

    cange test3

--- test 3.py
+++ test 3.py
@@ -2,12 +2,23 @@
 
 # print(dir(str))
 
-class Character:
-    name = ''
-    power = 0
-    energy = 100
-    hands = 2
+from typing_extensions import Self
 
+
+class Character:
+    # name = ''
+    # power = 0
+    # energy = 100
+    # hands = 2
+    
+    
+    def __init__(self, name, power, energy=100, hands=2):
+        self.name = name
+        self.power = power
+        self.energy = energy
+        self.backpack = []
+        self.hands = hands
+      
     def eat(self, food):
         if self.energy < 100:
             self.energy += food
@@ -21,19 +32,30 @@ class Character:
         else:
             print('Too tired')
 
-    def chalengee_aliase(self, new_alias):
+    def chalenge_aliase(self, new_alias):
         print(self)
-        self.aliase = new_alias
+        self.alias = new_alias
+
+    def beat_up(self, foe):
+        if not isinstance(foe, Character):
+            return
+        if foe.power < self.power:
+            foe.status = 'defeated'
+            self.statuse = 'winner'
+        else:
+            print('Retreat!')
 
 
-peter = Character()
-peter.name = 'Peter Parker'
-peter.power = 70
+
+peter = Character('Peter Parker', 70)
+# peter.name = 'Peter Parker'
+# peter.power = 70
 peter.alias = 'Spider-Man'
+peter.backpack.append('Web-shooter')
 
-bruce = Character()
-bruce.name = 'Bruce Wayne'
-bruce.power = 85
+bruce = Character('Bruce Wayne', 85)
+# bruce.name = 'Bruce Wayne'
+# bruce.power = 85
 bruce.alias = 'Batman'
 
 # print(peter.name)
@@ -41,4 +63,10 @@ bruce.alias = 'Batman'
 # print(peter.energy)
 # print(peter.hands)
 
+bruce.chalenge_aliase('Dark Knight')
+bruce.do_exercise(10)
+
+# print(bruce.power)
+# print(bruce.energy)
+# print(bruce.alias)
 
